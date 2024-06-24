ce9e240908491e078d3284365c4b089da208af9c
DarkzzXD <164411203+DarkzzXD@users.noreply.github.com>
2024-06-23 21:38:03 -0300

    Update orionlibrary.lua

--- orionlibrary.lua
+++ orionlibrary.lua
@@ -581,6 +581,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				}),
 				AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://4031889928"), {
 					Size = UDim2.new(1, 0, 1, 0),
+					Visible = false
 				}), "Second"),
 				MakeElement("Corner", 1)
 			}), "Divider"),
@@ -616,7 +617,8 @@ function OrionLib:MakeWindow(WindowConfig)
 
 	local WindowTopBarLine = AddThemeObject(SetProps(MakeElement("Frame"), {
 		Size = UDim2.new(1, 0, 0, 1),
-		Position = UDim2.new(0, 0, 1, -1)
+		Position = UDim2.new(0, 0, 1, -1),
+		Visible = false
 	}), "Stroke")
 
 	local MainWindow = AddThemeObject(SetChildren(SetProps(MakeElement("RoundFrame", Color3.fromRGB(255, 255, 255), 0, 10), {
